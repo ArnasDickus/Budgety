59ce24755825a419c89a50ea14f20f3e61bb8494
Arnas <1arnasdickus1@gmail.com>
2018-11-06 19:42:32 +0200

    Added template, configured to be working

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,11 @@
+// jquery is used for bootstrap
+import $ from "jquery";
+import 'bootstrap';
+
+
+
+import Copyright from "./modules/Copyright";
+
+
+let copyright = new Copyright();
+// Testing-comments
