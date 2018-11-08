dc36aa986887c5ff490d4dc8603541454333ed61
Arnas <1arnasdickus1@gmail.com>
2018-11-08 14:24:44 +0200

    input-data-completed

--- app/assets/scripts/modules/Budgety.js
+++ app/assets/scripts/modules/Budgety.js
@@ -13,23 +13,49 @@ class budgety{
         })();
         // UI CONTROLLER
         let UIController = (function(){
+            // Creating data structure if variables name changes.
+            let DOMstrings = {
+                inputType:        '.add__type',
+                inputDescription: '.add__description',
+                inputValue:       '.add__value',
+                inputButton:      '.add__btn'
+            }
+
+            return {
+                getInput: function() {
+
+                    return {
+                    type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                    description: document.querySelector(DOMstrings.inputDescription).value,
+                    value: document.querySelector(DOMstrings.inputValue).value
+                    };
+                },
+                getDOMstrings: function(){
+                    return DOMstrings;
+                }
+            };
             
         })();
 
         // GLOBAL APP CONTROLLER
         let controller = (function(budgetCtrl, UICtrl){
 
+        let DOM = UICtrl.getDOMstrings();
+        
         let ctrlAddItem = function(){
             //  TO DO List
             // 1.Get the filed input data
+            let input = UICtrl.getInput();
+            console.log(input);
+
             // 2. Add the item to the budget controlller
             // 3. Add the item to the UI.
             // 4. Calculate the budget.
             // 5. Display The budget on the UI
-            console.log("hello");
+
         } 
 
-           document.querySelector(".add__btn").addEventListener('click', ctrlAddItem);
+           document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
                
             
            document.addEventListener('keypress', function(event){
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -17061,21 +17061,44 @@ function () {
       }(); // UI CONTROLLER
 
 
-      var UIController = function () {}(); // GLOBAL APP CONTROLLER
+      var UIController = function () {
+        // Creating data structure if variables name changes.
+        var DOMstrings = {
+          inputType: '.add__type',
+          inputDescription: '.add__description',
+          inputValue: '.add__value',
+          inputButton: '.add__btn'
+        };
+        return {
+          getInput: function getInput() {
+            return {
+              type: document.querySelector(DOMstrings.inputType).value,
+              // Will be either inc or exp
+              description: document.querySelector(DOMstrings.inputDescription).value,
+              value: document.querySelector(DOMstrings.inputValue).value
+            };
+          },
+          getDOMstrings: function getDOMstrings() {
+            return DOMstrings;
+          }
+        };
+      }(); // GLOBAL APP CONTROLLER
 
 
       var controller = function (budgetCtrl, UICtrl) {
+        var DOM = UICtrl.getDOMstrings();
+
         var ctrlAddItem = function ctrlAddItem() {
           //  TO DO List
           // 1.Get the filed input data
-          // 2. Add the item to the budget controlller
+          var input = UICtrl.getInput();
+          console.log(input); // 2. Add the item to the budget controlller
           // 3. Add the item to the UI.
           // 4. Calculate the budget.
           // 5. Display The budget on the UI
-          console.log("hello");
         };
 
-        document.querySelector(".add__btn").addEventListener('click', ctrlAddItem);
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
         document.addEventListener('keypress', function (event) {
           //    Event.which for older browsers support.
           if (event.keyCode === 13 || event.which === 13) {
